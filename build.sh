b6b13e8bb80e38a08f5ddc50d6c70347241a92a2
yifengyou <842056007@qq.com>
2021-01-16 11:29:14 +0800

    add build.sh,.gitignore

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+set -xe
 #patch -p1 < ./u-boot-1.1.6_jz2440.patch
+make   100ask24x0_config
+make -j$(cat /proc/cpuinfo |grep processor|wc -l)
+
+if [ $? -eq 0 ];then
+	echo "Success"
+	ls -alh u-boot.bin
+else
+	echo "Failed"
+	exit 1
+fi
